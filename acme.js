89e88eadc8ca463270d9cfd9c93341cdf7297457
Johan Sellström <johan@sellstrom.me>
2021-02-26 22:10:29 +0100

    sample key

--- acme.js
+++ acme.js
@@ -70,7 +70,7 @@ const acmeKey = jose.JWK.asKey(fs.readFileSync('etc/privkey.pem'))
 
 app.get('/', (req, res) => {
     //var claims = {"svipeid": {"essential":true}, "given_name":null, "family_name":null};
-    var claims = {"svipeid": {"essential":true}};
+    var claims = {} //{"svipeid": {"essential":true}};
     console.log(claims);
     var redirect_uri = host+"/callback"; 
     var logo = host + "/logo.png";
